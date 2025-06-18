0f02d6fe924acd7765aa74edca87aa46b23ef942
claranogrmoura <claranogueirareis75@gmail.com>
2025-06-18 20:40:10 -0300

    up

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => { ////arvore do código
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/aulapwa-lista/sw.js")
       .then((registration) => {
         console.log("Service Worker registrado com sucesso:", registration);
       })
